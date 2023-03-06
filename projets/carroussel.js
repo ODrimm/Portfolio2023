de7b67353ceb4a311dabf327cc62b4f021b1c8f8
ODrimm <ystos.gaming@gmail.com>
2023-03-06 14:36:15 +0100

    mobile v1 page projets

--- projets/carroussel.js
+++ projets/carroussel.js
@@ -20,132 +20,60 @@ function CarousselDown() {
 }
 
 function CarousselGen(carousselPos) {
-
-    if (carousselPos == 0) {
-        carousselElements[0].style.top = 50 + "%"
-        carousselElements[1].style.top = 85 + "%"
-        carousselElements[2].style.top = 120 + "%"
-        carousselElements[3].style.top = 120 + "%"
-        carousselElements[4].style.top = 120 + "%"
-        carousselElements[5].style.top = 120 + "%"
-        carousselElements[6].style.top = 120 + "%"
-
-        for(let i = 0; i < carousselElements.length; i++){
-            carousselElements[i].classList.remove("mainCaroussel")
-            buttons[i].classList.remove("mainBouton")
-
+    if (window.matchMedia("(max-width: 800px)").matches) {
+        
+        for (let h = 0; h < carousselElements.length; h++) {
+            carousselElements[h].classList.remove("mainCaroussel")
+            buttons[h].classList.remove("mainBouton")
         }
-        carousselElements[0].classList.add("mainCaroussel")
-        buttons[0].classList.add("mainBouton")
 
-    }
-    else if (carousselPos == 1) {
-        carousselElements[0].style.top = 15 + "%"
-        carousselElements[1].style.top = 50 + "%"
-        carousselElements[2].style.top = 85 + "%"
-        carousselElements[3].style.top = 120 + "%"
-        carousselElements[4].style.top = 120 + "%"
-        carousselElements[5].style.top = 120 + "%"
-        carousselElements[6].style.top = 120 + "%"
-
-        for(let i = 0; i < carousselElements.length; i++){
-            carousselElements[i].classList.remove("mainCaroussel")
-            buttons[i].classList.remove("mainBouton")
-
+        for (let i = 0; i < carousselElements.length; i++) {
+            if (i == carousselPos) {
+                carousselElements[i].style.left = 40 + "%"
+                carousselElements[i].classList.add("mainCaroussel")
+                buttons[i].classList.add("mainBouton")
+            }else{
+                
+                if(i == carousselPos + 1){
+                    carousselElements[i].style.left = 95 + "%"
+                }else if(i > carousselPos + 1){
+                    carousselElements[i].style.left = 160 + "%"
+                }else if(i == carousselPos - 1){
+                    carousselElements[i].style.left = -15 + "%"
+                }else if(i < carousselPos - 1){
+                    carousselElements[i].style.left = -40 + "%"
+                }
+            }
         }
-        carousselElements[1].classList.add("mainCaroussel")
-        buttons[1].classList.add("mainBouton")
+        
 
-    }
-    else if (carousselPos == 2) {
-        carousselElements[0].style.top = -20 + "%"
-        carousselElements[1].style.top = 15 + "%"
-        carousselElements[2].style.top = 50 + "%"
-        carousselElements[3].style.top = 85 + "%"
-        carousselElements[4].style.top = 120 + "%"
-        carousselElements[5].style.top = 120 + "%"
-        carousselElements[6].style.top = 120 + "%"
+    } else {
 
-        for(let i = 0; i < carousselElements.length; i++){
-            carousselElements[i].classList.remove("mainCaroussel")
-            buttons[i].classList.remove("mainBouton")
-
+        for (let h = 0; h < carousselElements.length; h++) {
+            carousselElements[h].classList.remove("mainCaroussel")
+            buttons[h].classList.remove("mainBouton")
         }
-        carousselElements[2].classList.add("mainCaroussel")
-        buttons[2].classList.add("mainBouton")
 
-    }
-    else if (carousselPos == 3) {
-        carousselElements[0].style.top = -20 + "%"
-        carousselElements[1].style.top = -20 + "%"
-        carousselElements[2].style.top = 15 + "%"
-        carousselElements[3].style.top = 50 + "%"
-        carousselElements[4].style.top = 85 + "%"
-        carousselElements[5].style.top = 120 + "%"
-        carousselElements[6].style.top = 120 + "%"
-
-        for(let i = 0; i < carousselElements.length; i++){
-            carousselElements[i].classList.remove("mainCaroussel")
-            buttons[i].classList.remove("mainBouton")
-
+        for (let i = 0; i < carousselElements.length; i++) {
+            if (i == carousselPos) {
+                carousselElements[i].style.top = 50 + "%"
+                carousselElements[i].classList.add("mainCaroussel")
+                buttons[i].classList.add("mainBouton")
+            }else{
+                
+                if(i == carousselPos + 1){
+                    carousselElements[i].style.top = 85 + "%"
+                }else if(i > carousselPos + 1){
+                    carousselElements[i].style.top = 120 + "%"
+                }else if(i == carousselPos - 1){
+                    carousselElements[i].style.top = 15 + "%"
+                }else if(i < carousselPos - 1){
+                    carousselElements[i].style.top = -20 + "%"
+                }
+            }
         }
-        carousselElements[3].classList.add("mainCaroussel")
-        buttons[3].classList.add("mainBouton")
-
+        
     }
-    else if (carousselPos == 4) {
-        carousselElements[0].style.top = -20 + "%"
-        carousselElements[1].style.top = -20 + "%"
-        carousselElements[2].style.top = -20 + "%"
-        carousselElements[3].style.top = 15 + "%"
-        carousselElements[4].style.top = 50 + "%"
-        carousselElements[5].style.top = 85 + "%"
-        carousselElements[6].style.top = 120 + "%"
 
-        for(let i = 0; i < carousselElements.length; i++){
-            carousselElements[i].classList.remove("mainCaroussel")
-            buttons[i].classList.remove("mainBouton")
-
-        }
-        carousselElements[4].classList.add("mainCaroussel")
-        buttons[4].classList.add("mainBouton")
-
-    }
-    else if (carousselPos == 5) {
-        carousselElements[0].style.top = -20 + "%"
-        carousselElements[1].style.top = -20 + "%"
-        carousselElements[2].style.top = -20 + "%"
-        carousselElements[3].style.top = -20 + "%"
-        carousselElements[4].style.top = 15 + "%"
-        carousselElements[5].style.top = 50 + "%"
-        carousselElements[6].style.top = 85 + "%"
-
-        for(let i = 0; i < carousselElements.length; i++){
-            carousselElements[i].classList.remove("mainCaroussel")
-            buttons[i].classList.remove("mainBouton")
-
-        }
-        carousselElements[5].classList.add("mainCaroussel")
-        buttons[5].classList.add("mainBouton")
-
-    }
-    else if (carousselPos == 6) {
-        carousselElements[0].style.top = -20 + "%"
-        carousselElements[1].style.top = -20 + "%"
-        carousselElements[2].style.top = -20 + "%"
-        carousselElements[3].style.top = -20 + "%"
-        carousselElements[4].style.top = -20 + "%"
-        carousselElements[5].style.top = 15 + "%"
-        carousselElements[6].style.top = 50 + "%"
-
-        for(let i = 0; i < carousselElements.length; i++){
-            carousselElements[i].classList.remove("mainCaroussel")
-            buttons[i].classList.remove("mainBouton")
-
-        }
-        carousselElements[6].classList.add("mainCaroussel")
-        buttons[6].classList.add("mainBouton")
-
-
-    }
+    
 }
