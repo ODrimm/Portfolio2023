fbda9532bff5e6b6c41b1987e6bc4d2b9acc1eb7
ODrimm <ystos.gaming@gmail.com>
2023-03-08 09:26:57 +0100

    JS full responsive accueil + fix scroll projets

--- acceuil/lightTorch.js
+++ acceuil/lightTorch.js
@@ -1,13 +1,23 @@
 const circle = document.getElementById("circle"); //get circle div
 
-if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { //if is mobile device
 
-  const onMouseMove = (e) => { //move circle to cursor position
-    circle.style.transform = "translate(" + e.pageX + "px," +  e.pageY + "px";
+addEventListener("resize", (event) => {
+  if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { //if is desktop device
+
+    document.addEventListener('mousemove', onMouseMove); //mouve on mouse mov
+
+  }else{
+
+    circle.style.transform = "translate(" + 0 + "px," +  0 + "px";
+    document.removeEventListener('mousemove', onMouseMove);
+    
   }
+});
 
-  document.addEventListener('mousemove', onMouseMove); //mouve on mouse mov
+const onMouseMove = (e) => { //move circle to cursor position
+  circle.style.transform = "translate(" + e.pageX + "px," +  e.pageY + "px";
 }
+
 
 
 
