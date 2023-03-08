fe256bfee12aea307f0820b235aca446b16c1bfd
ODrimm <ystos.gaming@gmail.com>
2023-03-08 10:37:47 +0100

    fix acceuil light spawn

--- acceuil/lightTorch.js
+++ acceuil/lightTorch.js
@@ -1,5 +1,19 @@
 const circle = document.getElementById("circle"); //get circle div
 
+const onMouseMove = (e) => { //move circle to cursor position
+  circle.style.transform = "translate(" + e.pageX + "px," +  e.pageY + "px";
+}
+
+if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { //if is desktop device
+
+  document.addEventListener('mousemove', onMouseMove); //mouve on mouse mov
+
+}else{
+
+  circle.style.transform = "translate(" + 0 + "px," +  0 + "px";
+  document.removeEventListener('mousemove', onMouseMove);
+  
+}
 
 addEventListener("resize", (event) => {
   if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) { //if is desktop device
@@ -14,9 +28,7 @@ addEventListener("resize", (event) => {
   }
 });
 
-const onMouseMove = (e) => { //move circle to cursor position
-  circle.style.transform = "translate(" + e.pageX + "px," +  e.pageY + "px";
-}
+
 
 
 
