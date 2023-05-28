ce5a7b37cac6fdcd3cedd1d7542efbe2b67f7cff
ODrimm <ystos.gaming@gmail.com>
2023-05-28 20:32:15 +0200

    Category filters

--- projets/carroussel.js
+++ projets/carroussel.js
@@ -1,20 +1,30 @@
 let data;
 const content = document.querySelector(".content");
+const category = document.querySelector(".category");
+let currentCategory = "Tout";
+const categories = [];
 
 logJSONData();
-
 
 
 async function logJSONData() {
     const response = await fetch("../Assets/Data/data.json");
     const jsonData = await response.json();
     data = jsonData;
-    dataLog(jsonData);
+    generateProj(jsonData);
 }
 
-function dataLog(jsonData) {
+
+function generateProj(jsonData) {
 
     for (let i = 0; i < jsonData.length; i++) {
+        for (let j = 0; j < jsonData[i].category.length; j++) {
+            if (!categories.includes(jsonData[i].category[j])) {
+                categories.push(jsonData[i].category[j]);
+            }
+        }
+
+
         if (i == 0) {
             content.innerHTML += `
             <a href="${jsonData[i].link}" target="_blank" class="museum-element id${i} showMuseum" style="background-image: url('../Assets/Projets/${jsonData[i].imageLink}');">
@@ -34,23 +44,84 @@ function dataLog(jsonData) {
 
     }
 
+    category.innerHTML += `
+        <a class="category-element Tout" onclick="return categoryChange('Tout');">Tout</a>
+    `
+    for (let i = 0; i < categories.length; i++) {
+        category.innerHTML += `
+        <a class="category-element ${(categories[i].replace(/\s/g, '')).replace("3", "trois")}" onclick="return categoryChange('${categories[i]}');">${categories[i]}</a>
+        `
+    }
 }
 
-const docHeight = document.body.offsetHeight;
+function categoryChange(category) {
+    currentCategory = category;
+    content.innerHTML = "";
+    let unactiveCategories = document.querySelectorAll(".category-element");
+    for(let i = 0; i < unactiveCategories.length; i++){
+        unactiveCategories[i].classList.remove("active");
+    }
+    document.querySelector("." + (currentCategory.replace(/\s/g, '')).replace("3", "trois")).classList.add("active");
+    
+
+    for (let i = 0; i < data.length; i++) {
+
+        if (category == "Tout") {
+            if (content.innerHTML == "") {
+                content.innerHTML += `
+            <a href="${data[i].link}" target="_blank" class="museum-element id${i} showMuseum" style="background-image: url('../Assets/Projets/${data[i].imageLink}');">
+                <section class="museum-title">${data[i].description}</section>
+                <section class="museum-description">${data[i].title}</section>
+            </a>
+            `
+
+            } else {
+                content.innerHTML += `
+            <a href="${data[i].link}" target="_blank" class="museum-element id${i}" style="background-image: url('../Assets/Projets/${data[i].imageLink}');">
+                <section class="museum-title">${data[i].description}</section>
+                <section class="museum-description">${data[i].title}</section>
+            </a>
+            `
+            }
+        } else if (data[i].category.includes(category)) {
+
+            if (content.innerHTML == "") {
+                console.log("test");
+                content.innerHTML += `
+            <a href="${data[i].link}" target="_blank" class="museum-element id${i} showMuseum" style="background-image: url('../Assets/Projets/${data[i].imageLink}');">
+                <section class="museum-title">${data[i].description}</section>
+                <section class="museum-description">${data[i].title}</section>
+            </a>
+            `
+
+            } else {
+                content.innerHTML += `
+            <a href="${data[i].link}" target="_blank" class="museum-element id${i}" style="background-image: url('../Assets/Projets/${data[i].imageLink}');">
+                <section class="museum-title">${data[i].description}</section>
+                <section class="museum-description">${data[i].title}</section>
+            </a>
+            `
+            }
+        }
+
+
+
+    }
+}
 
 window.addEventListener("scroll", function () {
-    console.log(docHeight);
     if (data != undefined) {
         for (let i = 0; i < data.length; i++) {
-            let museumElement = document.querySelector(".id" + i);
-            let rect = museumElement.getBoundingClientRect();
-
-            if (75 < rect.top && rect.top < 400) {
-                museumElement.classList.add("showMuseum");
-            } else {
-                museumElement.classList.remove("showMuseum");
+            if(data[i].category.includes(currentCategory) || currentCategory == "Tout"){
+                let museumElement = document.querySelector(".id" + i);
+                let rect = museumElement.getBoundingClientRect();
+    
+                if (75 < rect.top && rect.top < 400) {
+                    museumElement.classList.add("showMuseum");
+                } else {
+                    museumElement.classList.remove("showMuseum");
+                }
             }
         }
     }
-
 });
