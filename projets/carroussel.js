9cd15fa97f4547b9d981627d388ecf2541cb02e3
ODrimm <ystos.gaming@gmail.com>
2023-05-28 11:04:03 +0200

    switch title et description + scroll height

--- projets/carroussel.js
+++ projets/carroussel.js
@@ -18,16 +18,16 @@ function dataLog(jsonData) {
         if (i == 0) {
             content.innerHTML += `
             <a href="${jsonData[i].link}" target="_blank" class="museum-element id${i} showMuseum" style="background-image: url('../Assets/Projets/${jsonData[i].imageLink}');">
-                <section class="museum-title">${jsonData[i].title}</section>
-                <section class="museum-description">${jsonData[i].description}</section>
+                <section class="museum-title">${jsonData[i].description}</section>
+                <section class="museum-description">${jsonData[i].title}</section>
             </a>
             `
 
         } else {
             content.innerHTML += `
             <a href="${jsonData[i].link}" target="_blank" class="museum-element id${i}" style="background-image: url('../Assets/Projets/${jsonData[i].imageLink}');">
-                <section class="museum-title">${jsonData[i].title}</section>
-                <section class="museum-description">${jsonData[i].description}</section>
+                <section class="museum-title">${jsonData[i].description}</section>
+                <section class="museum-description">${jsonData[i].title}</section>
             </a>
             `
         }
@@ -36,25 +36,21 @@ function dataLog(jsonData) {
 
 }
 
-let scollEvent = false;
+const docHeight = document.body.offsetHeight;
 
 window.addEventListener("scroll", function () {
+    console.log(docHeight);
+    if (data != undefined) {
+        for (let i = 0; i < data.length; i++) {
+            let museumElement = document.querySelector(".id" + i);
+            let rect = museumElement.getBoundingClientRect();
 
-    if(!scollEvent){
-        if (data != undefined) {
-            for (let i = 0; i < data.length; i++) {
-                let museumElement = document.querySelector(".id" + i);
-                console.log(museumElement);
-                let rect = museumElement.getBoundingClientRect();
-    
-                if (50 < rect.top && rect.top < 250) {
-                    museumElement.classList.add("showMuseum");
-                } else {
-                    museumElement.classList.remove("showMuseum");
-                }
+            if (75 < rect.top && rect.top < 400) {
+                museumElement.classList.add("showMuseum");
+            } else {
+                museumElement.classList.remove("showMuseum");
             }
         }
     }
-    scollEvent = !scollEvent;
 
 });
